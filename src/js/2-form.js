1af1992dc133cfc52cc8a3e8037a85cab062841d
Necrotic999 <l.ilya041105@gmail.com>
2024-03-01 22:48:23 +0200

    hw9 update files

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -4,7 +4,7 @@ const LS_KEY = "feedback-form-state";
 formEl.addEventListener("input", onFormElInput);
 document.addEventListener("DOMContentLoaded", () => {
     const data = loadFromLS(LS_KEY);
-
+    if (!data.email || !data.message) return;
     formEl.elements.email.value = data.email;
     formEl.elements.message.value = data.message;
 })
@@ -21,16 +21,12 @@ function onFormElInput(e) {
 
 function loadFromLS(key) { 
     const data = localStorage.getItem(key);
-    try {
-        return JSON.parse(data);
-    } catch { 
-        return data;
-    }
+    return JSON.parse(data);
 }
 
 function onFormElSubmit(e) { 
     e.preventDefault();
-    if (!formEl.elements.email.value || !formEl.elements.message.value) return;
+    if (!formEl.elements.email.value.trim() || !formEl.elements.message.value.trim()) return;
     console.log(loadFromLS(LS_KEY));
     localStorage.removeItem(LS_KEY);
     formEl.elements.email.value = "";
